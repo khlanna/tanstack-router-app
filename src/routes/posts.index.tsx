5055a7e3ca0a4384a99595d145ce7a37ed3ad6b6
Anna Khloyan <anna@jogl.io>
2025-10-28 12:28:59 +0400

    implement pagination

--- src/routes/posts.index.tsx
+++ src/routes/posts.index.tsx
@@ -1,22 +1,35 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { getPosts } from "../data/posts";
+import { z } from "zod";
+import { zodValidator } from "@tanstack/zod-adapter";
+
+const searchSchema = z.object({
+  page: z.number().default(1),
+});
 
 export const Route = createFileRoute("/posts/")({
   component: RouteComponent,
+  validateSearch: zodValidator(searchSchema),
   beforeLoad: () => {
     return {
       getPosts: getPosts,
     };
   },
-  loader: async ({ context }) => {
+  loaderDeps: ({ search }) => {
+    return {
+      page: search.page,
+    };
+  },
+  loader: async ({ context, deps }) => {
     const { getPosts } = context;
-    const posts = await getPosts();
+    const posts = await getPosts(deps.page);
     return { context, posts: posts.posts };
   },
 });
 
 function RouteComponent() {
   const { posts } = Route.useLoaderData();
+
   return (
     <div className="flex flex-col gap-2">
       Hello "/posts/"!
@@ -31,6 +44,24 @@ function RouteComponent() {
           </Link>
         </div>
       ))}
+      <div className="flex gap-4">
+        <Link
+          from={Route.fullPath}
+          to="."
+          search={(prev) => ({ ...prev, page: prev.page - 1 })}
+          className="text-yellow-500"
+        >
+          Go to Prev Page
+        </Link>
+        <Link
+          from={Route.fullPath}
+          to="."
+          search={(prev) => ({ ...prev, page: prev.page + 1 })}
+          className="text-yellow-500"
+        >
+          Go to Next Page
+        </Link>
+      </div>
     </div>
   );
 }
